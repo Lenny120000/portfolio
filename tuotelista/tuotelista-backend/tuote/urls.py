53833b25288946a0f15bba16d40bec9c8fb698e0
OivaM <82021204+Lenny120000@users.noreply.github.com>
2024-10-02 15:00:26 +0300

    Update urls.py

--- tuotelista/tuotelista-backend/tuote/urls.py
+++ tuotelista/tuotelista-backend/tuote/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from tuote import views
-from .views import LogoutView, TuoteViewSet
+from .views import TuoteViewSet#,ReactLoginView, LogoutView, 
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
@@ -17,10 +17,12 @@ urlpatterns = format_suffix_patterns([
     path('tuote/<int:pk>', views.TuoteDetail.as_view(), name='tuote-detail'),
     path('tuote/uploads/', views.TuoteImage.as_view(), name='tuote-image'),
     path('tuote/ReactView', views.ReactView.as_view(), name='tuote-react'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
-    path('api-auth/logout/', LogoutView.as_view(), name='logout'),
-    path('api-auth/', include('rest_framework.urls')),
-    path('admin/', admin.site.urls),
+    #path('users/', views.UserList.as_view(), name='user-list'),
+    #path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
+    #path('react/login/', ReactLoginView.as_view(), name='login'),
+    #path('auth/logout/', LogoutView.as_view(), name='logout'),
+    #path('auth/', include('rest_framework.urls')),
+    #path('admin/', admin.site.urls),
 ]) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+"""path('auth/login/', LoginView.as_view(), name='login'),"""
